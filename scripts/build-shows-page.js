1cd6629c0d127556ac42e0297eb964aa91f7b1d2
FirstName LastName <you@example.com>
2024-02-18 12:10:29 -0500

    finalized code to commit main branch

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,49 +1,30 @@
 
 
 
-const getShows = async () =>{
-
+const getShows = async () => {
    const userData = await BandSiteApiInstance.getShowData();
-   console.log(userData);
 
-    userData.forEach(show => {
-
-
+   userData.forEach(show => {
        displayShowDetails(show);
-
    });
-
-
-
-
 }
-
 getShows();
 
 
 
-
-
-
-
-
 const showsEl = document.querySelector(".shows");
-
 const deskTitleEl = document.createElement('div');
 deskTitleEl.classList.add("shows__main");
 showsEl.append(deskTitleEl);
-
 
 const showHeaderEl = document.createElement('div');
 showHeaderEl.classList.add("shows__header");
 deskTitleEl.append(showHeaderEl);
 
-
 const tabDateTitleEl = document.createElement('h4');
 tabDateTitleEl.innerText = "Date";
 tabDateTitleEl.classList.add("shows__heading-date")
 showHeaderEl.append(tabDateTitleEl);
-
 
 const tabVenueTitleEl = document.createElement('h4');
 tabVenueTitleEl.innerText = "Venue";
@@ -59,107 +40,73 @@ const tabExtraEl = document.createElement('p');
 tabExtraEl.classList.add("shows__heading-element")
 showHeaderEl.append(tabExtraEl);
 
-
-
-
-
 function displayShowDetails(show) {
+   const showsection = document.querySelector(".shows");
+   const showEl = document.createElement("article");
+   showEl.classList.add("shows__show");
+   deskTitleEl.append(showEl);
 
- const showsection = document.querySelector(".shows");
+   const showContainerEl = document.createElement('div');
+   showContainerEl.classList.add("shows__container");
+   showEl.append(showContainerEl);
 
+   const TabletEl = document.createElement('div');
+   TabletEl.classList.add("shows__wrapper")
+   showEl.append(TabletEl);
 
- const showEl = document.createElement("article");
- showEl.classList.add("shows__show");
- deskTitleEl.append(showEl);
+   const dateTitleEl = document.createElement('div');
+   dateTitleEl.innerText = "Date";
+   dateTitleEl.classList.add("shows__subtitle")
+   TabletEl.append(dateTitleEl);
 
- const showContainerEl = document.createElement('div');
- showContainerEl.classList.add("shows__container");
- showEl.append(showContainerEl);
+   const showDateEl = document.createElement("p");
+   showDateEl.classList.add("shows__date");
 
-//  const showlistEl = document.createElement("ul");
-//  showlistEl.classList.add("shows__lists");
-//  showContainerEl.append(showlistEl);
+   let milliseconds = show.date;
+   let showDate = new Date(milliseconds);
+   const currentDayOfMonth = showDate.getDate();
+   const currentMonth = showDate.getMonth();
+   const currentYear = showDate.getFullYear();
+   const newShowDate = currentDayOfMonth + "/" + (currentMonth + 1) + "/" + currentYear;
+   showDateEl.innerText = newShowDate;
+   TabletEl.append(showDateEl);
 
+   const venueTitleEl = document.createElement('div');
+   venueTitleEl.innerText = "Venue";
+   venueTitleEl.classList.add("shows__subtitle")
+   TabletEl.append(venueTitleEl);
 
- const TabletEl = document.createElement('div');
- TabletEl.classList.add("shows__wrapper")
- showEl.append(TabletEl);
+   const showVenueEl = document.createElement("p");
+   showVenueEl.classList.add("shows__venue");
+   showVenueEl.innerText = show.place;
+   TabletEl.append(showVenueEl);
 
+   const locationTitleEl = document.createElement('div');
+   locationTitleEl.innerText = "Location";
+   locationTitleEl.classList.add("shows__subtitle")
+   TabletEl.append(locationTitleEl);
 
- const dateTitleEl = document.createElement('div');
- dateTitleEl.innerText = "Date";
- dateTitleEl.classList.add("shows__subtitle")
- TabletEl.append(dateTitleEl);
+   const showLocationEl = document.createElement("p");
+   showLocationEl.classList.add("shows__location");
+   showLocationEl.innerText = show.location;
+   TabletEl.append(showLocationEl);
 
+   var btnEl = document.createElement('button');
+   btnEl.textContent = 'Buy Tickets';
+   btnEl.classList.add("shows__button");
+   TabletEl.append(btnEl);
 
- const showDateEl = document.createElement("p");
- showDateEl.classList.add("shows__date");
+   let divider = document.createElement("hr");
+   divider.classList.add("shows__divider");
+   showEl.append(divider);
 
- let milliseconds = show.date;
- //console.log (milliseconds);
- let showDate = new Date(milliseconds);
-// console.log("Milliseconds = " + date);
- const currentDayOfMonth = showDate.getDate();
- const currentMonth = showDate.getMonth();
- const currentYear = showDate.getFullYear();
+   //this listener for color changing when clicking
+   showEl.addEventListener('click', function(event) {
+       event.preventDefault();
+       if (document.querySelector(".shows__show--selected")) {
+           document.querySelector(".shows__show--selected").classList.remove('shows__show--selected');
+       }
 
- const newShowDate = currentDayOfMonth + "/" + (currentMonth + 1) + "/" + currentYear;
-
- showDateEl.innerText = newShowDate;
-
- TabletEl.append(showDateEl);
-
-
- const venueTitleEl = document.createElement('div');
- venueTitleEl.innerText = "Venue";
- venueTitleEl.classList.add("shows__subtitle")
- TabletEl.append(venueTitleEl);
-
-
- const showVenueEl = document.createElement("p");
- showVenueEl.classList.add("shows__venue");
- showVenueEl.innerText = show.place;
- TabletEl.append(showVenueEl);
-
- const locationTitleEl = document.createElement('div');
- locationTitleEl.innerText = "Location";
- locationTitleEl.classList.add("shows__subtitle")
- TabletEl.append(locationTitleEl);
-
- const showLocationEl = document.createElement("p");
- showLocationEl.classList.add("shows__location");
- showLocationEl.innerText = show.location;
- TabletEl.append(showLocationEl);
-
-
- var btnEl = document.createElement('button');
- btnEl.textContent = 'Buy Tickets';
- btnEl.classList.add("shows__button");
- TabletEl.append(btnEl);
-
-
- let divider = document.createElement("hr");
-
- divider.classList.add("shows__divider");
-
- showEl.append(divider);
-
-
-
-
- showEl.addEventListener('click', function (event) {
-
-    event.preventDefault();
-    if (document.querySelector(".shows__show--selected")) {
-
-       document.querySelector(".shows__show--selected").classList.remove('shows__show--selected');
-
-    }
-
-    console.log(showEl);
-
-    showEl.classList.add('shows__show--selected');
-
- });
-
+       showEl.classList.add('shows__show--selected');
+   });
 }
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,205 +1,114 @@
 const commentssection = document.querySelector(".comments");
 
-
-
-
 const reviewcontainer = document.createElement('div');
 reviewcontainer.classList.add("comments__bottom");
 commentssection.append(reviewcontainer);
 
-
 function displayReviewDetails(userComment) {
+    const review = document.createElement("figure");
+    review.classList.add("review");
+    reviewcontainer.appendChild(review);
 
-   //const reviewcontainer1 = document.querySelector("comments__bottom");
+    let horizonalline = document.createElement("hr");
+    horizonalline.classList.add("review__line");
+    review.appendChild(horizonalline);
 
-   const review = document.createElement("figure");
-   review.classList.add("review");
-   reviewcontainer.appendChild(review);
+    const reviewWrapper = document.createElement('div');
+    reviewWrapper.classList.add("review__wrapper")
+    review.appendChild(reviewWrapper);
 
-   let horizonalline = document.createElement("hr");
-   horizonalline.classList.add("review__line");
-   review.appendChild(horizonalline);
+    const reviewAvatar = document.createElement('div');
+    reviewAvatar.classList.add("review__placeholder");
+    reviewAvatar.src = "";
+    reviewWrapper.appendChild(reviewAvatar);
 
-   const reviewWrapper = document.createElement('div');
-   reviewWrapper.classList.add("review__wrapper")
-   review.appendChild(reviewWrapper);
+    const reviewRightContainer = document.createElement('div');
+    reviewRightContainer.classList.add("review__right-container")
+    reviewWrapper.appendChild(reviewRightContainer);
 
+    const reviewtextWrapper = document.createElement('div');
+    reviewtextWrapper.classList.add("review__text-container")
+    reviewRightContainer.appendChild(reviewtextWrapper);
 
-   const reviewAvatar = document.createElement('div');
-   reviewAvatar.classList.add("review__placeholder");
-   reviewAvatar.src = "";
+    const reviewname = document.createElement('h4');
+    reviewname.classList.add("review__name");
+    reviewname.innerText = userComment.name;
+    reviewtextWrapper.appendChild(reviewname);
 
-   reviewWrapper.appendChild(reviewAvatar);
-
-   const reviewRightContainer = document.createElement('div');
-   reviewRightContainer.classList.add("review__right-container")
-   reviewWrapper.appendChild(reviewRightContainer);
-
-   const reviewtextWrapper = document.createElement('div');
-   reviewtextWrapper.classList.add("review__text-container")
-   reviewRightContainer.appendChild(reviewtextWrapper);
-
-   const reviewname = document.createElement('h4');
-   reviewname.classList.add("review__name");
-   reviewname.innerText = userComment.name;
-
-   reviewtextWrapper.appendChild(reviewname);
-
-   const reviewDate = document.createElement("time");
-   reviewDate.classList.add("review__date");
-
+    const reviewDate = document.createElement("time");
+    reviewDate.classList.add("review__date");
     let milliseconds = userComment.timestamp;
-    //console.log (milliseconds);
+
     let ReviewDate = new Date(milliseconds);
-   // console.log("Milliseconds = " + date);
     const currentDayOfMonth = ReviewDate.getDate();
     const currentMonth = ReviewDate.getMonth();
     const currentYear = ReviewDate.getFullYear();
-
     const newReviewDate = currentDayOfMonth + "/" + (currentMonth + 1) + "/" + currentYear;
+    reviewDate.innerText = newReviewDate;
+    reviewtextWrapper.appendChild(reviewDate);
 
+    const reviewMessageContainer = document.createElement('div');
+    reviewMessageContainer.classList.add("review__comment-container")
+    reviewRightContainer.appendChild(reviewMessageContainer);
 
-
-
-
-reviewDate.innerText = newReviewDate;
-   reviewtextWrapper.appendChild(reviewDate);
-
-   const reviewMessageContainer = document.createElement('div');
-   reviewMessageContainer.classList.add("review__comment-container")
-   reviewRightContainer.appendChild(reviewMessageContainer);
-
-
-   const reviewmessage = document.createElement('p');
-   reviewmessage.classList.add("review__sentence")
-   reviewmessage.innerText = userComment.comment;
-   reviewMessageContainer.appendChild(reviewmessage);
-
-
+    const reviewmessage = document.createElement('p');
+    reviewmessage.classList.add("review__sentence")
+    reviewmessage.innerText = userComment.comment;
+    reviewMessageContainer.appendChild(reviewmessage);
 }
 
-
-
-const getComments = async () =>{
-
-   try {
-
-   const userData = await BandSiteApiInstance.getData();
-
- const reviewData = userData.sort(function(a, b) {
-       return b.timestamp - a.timestamp;
-    })
-
-    reviewData.forEach(userComment => {
-
-
-  displayReviewDetails(userComment);
-
-
-   });
-   }
-
-   catch (error){
-
-   }
-
+//Get comments from api to page
+const getComments = async () => {
+    try {
+        const userData = await BandSiteApiInstance.getData();
+        const reviewData = userData.sort(function(a, b) {
+            return b.timestamp - a.timestamp;
+        })
+        reviewData.forEach(userComment => {
+            displayReviewDetails(userComment);
+        });
+    } catch (error) {}
 }
 getComments();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let horizonalline1 = document.createElement("hr");
-// horizonalline1.classList.add("review__line-bottom");
-// commentssection.appendChild(horizonalline1);
-
-const postComments = async ({fullName,reviewText}) =>{
-
-   try{
-
-    const post1Data = await BandSiteApiInstance.postData(fullName,reviewText);
-
-
-      //Get Comments
-      getComments();
-
-
-   }
-   catch(error) {
-
-   }
-
-
-
-
-  }
-
-
-
-
-
+//Post comments to api
+const postComments = async ({
+    fullName,
+    reviewText
+}) => {
+    try {
+        const post1Data = await BandSiteApiInstance.postData(fullName, reviewText);
+        getComments();
+    } catch (error) {}
+}
 
 
 
 const reviewFormEl = document.forms.reviewform;
 const btnEl = document.querySelector(".comments__button")
-
 const textBox = document.querySelector(".comments__name-textBox");
-const messageArea = document.querySelector("comments__textarea");
+ const messageArea = document.querySelector("comments__textarea");
 
 
-
-
-
- reviewFormEl.addEventListener ("submit", (event) => {
-
-
-   event.preventDefault();
-
-
+reviewFormEl.addEventListener("submit", (event) => {
+    event.preventDefault();
     const fullNameEl = event.target.fullName.value;
     const reviewTextEl = event.target.reviewText.value;
-
-
-
-
-
-    postComments({fullName:fullNameEl,reviewText:reviewTextEl});
-
+    postComments({
+        fullName: fullNameEl,
+        reviewText: reviewTextEl
+    });
     event.target.fullName.value = "";
     event.target.reviewText.value = "";
-
-
     clearComment()
+});
 
 
+const fullReviewPage = document.querySelector('.comments__bottom')
 
-
-
-
-
- });
-
- const fullReviewPage = document.querySelector('.comments__bottom')
-
-
- function clearComment(){
-   while (fullReviewPage.firstChild) {
-      fullReviewPage.removeChild(fullReviewPage.firstChild);
-  }
-
- }
+function clearComment() {
+    while (fullReviewPage.firstChild) {
+        fullReviewPage.removeChild(fullReviewPage.firstChild);
+    }
+}
